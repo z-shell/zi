4eb012e0b35736112b7ab44bf90808aa9690cd95
Salvydas Lukosius <sall@w-ss.io>
2021-11-17 03:49:11 +0000

    Fixes issue where Neovim uses `macos`

--- lib/zsh/install.zsh
+++ lib/zsh/install.zsh
@@ -1432,7 +1432,7 @@ builtin source "${ZINIT[BIN_DIR]}/lib/zsh/side.zsh" || { builtin print -P "${ZIN
         aarch64 "aarch64"
         aarch64-2 "arm"
         linux   "(linux|linux-gnu)"
-        darwin  "(darwin|mac|osx|os-x)"
+        darwin  "(darwin|mac|macos|osx|os-x)"
         cygwin  "(windows|cygwin|[-_]win|win64|win32)"
         windows "(windows|cygwin|[-_]win|win64|win32)"
         msys "(windows|msys|cygwin|[-_]win|win64|win32)"

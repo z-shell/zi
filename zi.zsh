77b41ea009ade61426100fab655dc3143e4aaa6e
Salvydas Lukosius <sall@w-ss.io>
2021-12-07 16:18:06 +0000

    case fix

--- zi.zsh
+++ zi.zsh
@@ -2622,6 +2622,7 @@ You can try to prepend {apo}${___q}{lhi}@{apo}'{error} to the ID if the last ice
           ;;
         (zt)
           .zi-turbo
+          ;;
         (ls)
           shift
           .zi-ls "$@"

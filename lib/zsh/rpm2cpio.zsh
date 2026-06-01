81a1cf5e7b9d719410777830a3c4c62c2842602e
Salvydas Lukosius <ss-o@users.noreply.github.com>
2026-06-01 02:42:21 +0100

    chore(zsh): apply standard modelines across plugins and libraries

--- lib/zsh/rpm2cpio.zsh
+++ lib/zsh/rpm2cpio.zsh
@@ -1,4 +1,6 @@
 #!/usr/bin/env zsh
+# -*- mode: zsh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
+# vim: ft=zsh sw=2 ts=2 et
 
 builtin emulate -R zsh ${=${options[xtrace]:#off}:+-o xtrace}
 builtin setopt extended_glob
